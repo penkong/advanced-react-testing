ec448d3343b7ff8cbd07cf97eb3c681185bc7047
mazdak <nazemi.works@gmail.com>
2019-05-29 07:02:59 -0700

    desc group up it func related to certain

--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -10,23 +10,28 @@ it('should has a textarea and a button', () => {
   expect(wrapped.find('textarea').length).toEqual(1);
   expect(wrapped.find('button').length).toEqual(1);
 });
+//desc group tests in a func and has own beforeEach
+describe('the textarea', () => {
+  beforeEach(()=>{
+    //fake simulate for changes of event
+    wrapped.find('textarea').simulate('change', { //this obj go merge with real obj in event handler
+      target: { value: 'new comment' }
+    });
+  });
+  it('should has a textarea can type in', () => {
+     //force update
+    wrapped.update(); //this guy take out props
+    expect(wrapped.find('textarea').prop('value')).toEqual('new comment');
+  });
 
-it('should has a textarea can type in', () => {  
-  //fake simulate for changes of event
-  wrapped.find('textarea').simulate('change',{ //this obj go merge with real obj in event handler
-    target: {value : 'new comment'}
-  }); //force update
-  wrapped.update();            //this guy take out props
-  expect(wrapped.find('textarea').prop('value')).toEqual('new comment');
-});
+  it('should has a form clear textarea after submit', () => {
+    wrapped.update();
+    wrapped.find('form').simulate('submit'); //exec submit indeed
+    //because of async notation of setState
+    wrapped.update();
+    expect(wrapped.find('textarea').prop('value')).toEqual('');
+  });
+})
 
-it('should has a form clear textarea after submit', () => {  
-  wrapped.find('textarea').simulate('change',{
-    target: { value : 'new comment' }
-  }); 
-  //because of async notation of setState
-  wrapped.update();
-  wrapped.find('form').simulate('submit');//exec submit indeed
-  wrapped.update();
-  expect(wrapped.find('textarea').prop('value')).toEqual('');
-});
+
+
