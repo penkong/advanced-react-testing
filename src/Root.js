95d1705d23dc7abbbb68499888905539d83fcbfb
mazdak <nazemi.works@gmail.com>
2019-05-30 03:31:43 -0700

    add fetch with axios and moxios and redux promise middleware

--- src/Root.js
+++ src/Root.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import reduxPromise from 'redux-promise';
+
 import reducers from 'reducers';
 //we did this comp for test files can access to store for test
 //handling
 export default ({children, initialState = {}}) => {
-  const store = createStore(reducers, initialState);
+  const store = createStore(reducers, initialState, applyMiddleware(reduxPromise));
   return (
     <Provider store={store}>
       {children}
