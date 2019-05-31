74559d84a1e85e1f4262579c15a4c767a342dfc0
mazdak <nazemi.works@gmail.com>
2019-05-31 05:56:10 -0700

    add login logic in services by localStrategy

--- auth/server/router.js
+++ auth/server/router.js
@@ -1,5 +1,13 @@
 const Authentication = require('./controllers/authentication');
+const passportService = require('./services/passport'); //check user for us is singin or not
+const passport = require('passport');
+
+//that middle ware we want to use to check
+const requireAuth = passport.authenticate('jwt', { session : false });
 
 module.exports = function (app) {
+  app.get('/', requireAuth , function (req,res){
+    res.send({ hi : 'there'});
+  });
   app.post('/signup', Authentication.singup);
 }
--- auth/server/services/passport.js
+++ auth/server/services/passport.js
@@ -5,9 +5,21 @@ const passport = require('passport');
 const User = require('../models/user');
 const config = require('../config/config');
 //strategy is method for authenticate a user
-const JwtStrategy = require('passport-jwt').Strategy; 
 //verify usr jwt , verify user with name and pass
+const JwtStrategy = require('passport-jwt').Strategy; 
 const ExtractJwt = require('passport-jwt').ExtractJwt; 
+const LocalStrategy = require('passport-local');
+
+//create localStrategy for sign in user
+const localOptions = { usernameField: email}
+const localLogin = new LocalStrategy(localOptions , function(email, password, done){
+  //verify user and pass call done if it correct otherwise call done with false
+  User.findOne({ email }, function(err,user){
+    if (err) return done(err);
+    if (!user) return done(null, false);
+    
+  });
+});
 
 //setup opt for jwt strategy
  //... we say where to look to find jwt of user
