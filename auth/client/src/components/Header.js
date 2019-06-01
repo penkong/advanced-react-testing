6480a63b84817c972483744a0aee266b161617fa
mazdak <nazemi.works@gmail.com>
2019-06-01 07:21:27 -0700

    good boilerplate header done proj almost done

--- auth/client/src/components/Header.js
+++ auth/client/src/components/Header.js
@@ -1,21 +1,35 @@
+import './HeaderStyle.css';
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-
+import { connect } from 'react-redux';
 class Header extends Component {
   renderLinks(){
-    
+    if(this.props.authenticated){
+      return (
+        <div>
+          <Link to="/signout">Sign Out</Link>
+          <Link to="/feature">Feature</Link>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <Link to="/signup">Sign Up</Link>
+          <Link to="/signin">Sign In</Link>
+        </div>
+      );
+    }
   }
   render() {
     return (
-      <div>
+      <div className="header">
         <Link to="/">Redux auth app</Link>
-        <Link to="/signup">Sign Up</Link>
-        <Link to="/signin">Sign In</Link>
-        <Link to="/signout">Sign Out</Link>
-        <Link to="/feature">Feature</Link>
+        {this.renderLinks()}
       </div>
     );
   }
 }
-
-export default Header;
+const mapStateToProps = (state) => {
+  return { authenticated: state.auth.authenticated };
+}
+export default connect(mapStateToProps)(Header);
