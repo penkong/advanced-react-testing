4755e54099d5a796023a6880431602f15a2f06cc
mazdak <nazemi.works@gmail.com>
2019-06-01 04:09:21 -0700

    add redux-form field reduxfom formReducer

--- auth/client/src/reducers/index.js
+++ auth/client/src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
+import { reducer as formReducer } from 'redux-form';
 import auth from './auth';
 
 
 
 export default combineReducers({
-  auth
+  auth,
+  form: formReducer
 });
